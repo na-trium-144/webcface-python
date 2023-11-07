a3fae1a5c0fb94c74c4853572775534e91407bd9
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-11-07 21:40:27 +0900

    コメント追加

--- example/main.py
+++ example/main.py
@@ -13,14 +13,20 @@ class A:
 
 def main():
     wcli = Client("example_main")
+
+    # 値を送信
     wcli.value("test").set(0)
+
     # wcli.value("dict").set(A().to_dict())
 
     def hello() -> None:
         print("hello, world!")
 
+    # 関数を登録
     wcli.func("func1").set(hello)
 
+    # デコレータを使って関数登録
+    # 引数の詳細な条件を設定することもできる
     @wcli.func(
         args=[Arg(min=0, max=10), Arg(), Arg(), Arg(option=["hoge", "fuga", "piyo"])]
     )
@@ -31,9 +37,12 @@ def main():
     i = 0
     while True:
         time.sleep(0.1)
+        # 値を更新
         wcli.value("test").set(i)
+        # 文字列を送信
         wcli.text("str").set("hello")
 
+        # viewを送信
         v = wcli.view("a")
         v.add(f"hello, world\n{i}\n")
         v.add(view.button("a", lambda: print("hello")))
