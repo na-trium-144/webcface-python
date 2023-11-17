dbdfc11f983df2d67780d2087770312024426366
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-11-17 21:19:27 +0900

    viewのドキュメント

--- webcface/view.py
+++ webcface/view.py
@@ -58,6 +58,14 @@ class ViewComponent(webcface.view_base.ViewComponentBase):
         text_color: int = 0,
         bg_color: int = 0,
     ) -> None:
+        """コンポーネントを作成
+
+        :arg type: コンポーネントの種類 (text(), button()などコンポーネントを作成する各種関数を使えば自動で設定される)
+        :arg text: 表示する文字列
+        :arg on_click: クリック時に実行する関数
+        :arg text_color: 文字の色 (ViewColorのEnumを使う)
+        :arg bg_color: 背景の色 (ViewColorのEnumを使う)
+        """
         super().__init__(type, text, None, text_color, bg_color)
         self._data = None
         self._on_click_func = None
@@ -74,6 +82,7 @@ class ViewComponent(webcface.view_base.ViewComponentBase):
     def lock_tmp(
         self, data: webcface.client_data.ClientData, field_id: str
     ) -> ViewComponent:
+        """AnonymousFuncをFuncオブジェクトにlockする"""
         if self._on_click_func_tmp is not None:
             on_click = webcface.func.Func(
                 webcface.field.Field(data, data.self_member_name), field_id
@@ -85,6 +94,10 @@ class ViewComponent(webcface.view_base.ViewComponentBase):
         return self
 
     def __eq__(self, other) -> bool:
+        """プロパティの比較
+
+        :return: プロパティが全部等しければTrueになる
+        """
         return (
             isinstance(other, ViewComponent)
             and self._type == other._type
@@ -107,14 +120,20 @@ class ViewComponent(webcface.view_base.ViewComponentBase):
 
     @property
     def type(self) -> int:
+        """コンポーネントの種類
+
+        ViewComponentType Enumを使う
+        """
         return self._type
 
     @property
     def text(self) -> str:
+        """表示する文字列"""
         return self._text
 
     @property
     def on_click(self) -> Optional[webcface.func.Func]:
+        """クリックしたときに呼び出す関数"""
         if self._on_click_func is not None:
             if self._data is None:
                 raise RuntimeError("internal data not set")
@@ -127,24 +146,44 @@ class ViewComponent(webcface.view_base.ViewComponentBase):
 
     @property
     def text_color(self) -> int:
+        """文字の色
+
+        ViewColor Enumを使う
+        """
         return self._text_color
 
     @property
     def bg_color(self) -> int:
+        """背景の色
+
+        ViewColor Enumを使う
+        """
         return self._bg_color
 
 
 def text(text: str, **kwargs) -> ViewComponent:
+    """textコンポーネント
+
+    kwargsに指定したプロパティはViewComponentのコンストラクタに渡される
+    """
     return ViewComponent(type=ViewComponentType.TEXT, text=text, **kwargs)
 
 
 def new_line() -> ViewComponent:
+    """newLineコンポーネント
+
+    kwargsに指定したプロパティはViewComponentのコンストラクタに渡される
+    """
     return ViewComponent(type=ViewComponentType.NEW_LINE)
 
 
 def button(
     text: str, on_click: webcface.func.Func | webcface.func.AnonymousFunc | Callable
 ) -> ViewComponent:
+    """buttonコンポーネント
+
+    kwargsに指定したプロパティはViewComponentのコンストラクタに渡される
+    """
     return ViewComponent(type=ViewComponentType.BUTTON, text=text, on_click=on_click)
 
 
@@ -152,31 +191,45 @@ class View(webcface.field.Field):
     _components: list[ViewComponent | str | bool | float | int]
 
     def __init__(self, base: webcface.field.Field, field: str = "") -> None:
+        """Viewを指すクラス
+
+        このコンストラクタを直接使わず、
+        Member.view(), Member.views(), Member.onViewEntry などを使うこと
+
+        詳細は `Viewのドキュメント <https://na-trium-144.github.io/webcface/md_13__view.html>`_ を参照
+        """
         super().__init__(base.data, base._member, field if field != "" else base._field)
         self._components = []
         if self.data.is_self(self._field):
             self.init()
 
-    def __del__(self) -> None:
-        if self.data.is_self(self._field):
-            self.sync()
-
     @property
     def member(self) -> webcface.member.Member:
+        """Memberを返す"""
         return webcface.member.Member(self)
 
     @property
     def name(self) -> str:
+        """field名を返す"""
         return self._field
 
     @property
     def signal(self) -> blinker.NamedSignal:
+        """値が変化したときのイベント
+
+        コールバックの引数にはViewオブジェクトが渡される。
+        """
         return self.data.signal("view_change", self._member, self._field)
 
     def child(self, field: str) -> View:
+        """子フィールドを返す
+
+        :return: 「(thisのフィールド名).(子フィールド名)」をフィールド名とするView
+        """
         return View(self, self._field + "." + field)
 
     def try_get(self) -> Optional[list[ViewComponent]]:
+        """ViewをlistまたはNoneで返す"""
         v = self.data.view_store.get_recv(self._member, self._field)
         v2: Optional[list[ViewComponent]] = None
         if v is not None:
@@ -184,10 +237,12 @@ class View(webcface.field.Field):
         return v2
 
     def get(self) -> list[ViewComponent]:
+        """Viewをlistで返す"""
         v = self.try_get()
         return v if v is not None else []
 
     def set(self, components: list[ViewComponent | str | bool | float | int]) -> View:
+        """Viewのリストをセットする"""
         self._set_check()
         data2 = []
         for c in components:
@@ -210,14 +265,20 @@ class View(webcface.field.Field):
         return self
 
     def init(self) -> View:
+        """このViewオブジェクトにaddした内容を初期化する
+
+        コンストラクタ内でも自動で呼ばれる
+        """
         self._components = []
         return self
 
     def sync(self) -> View:
+        """Viewの内容をclientに反映し送信可能にする"""
         self.set(self._components)
         return self
 
     def add(self, *args: ViewComponent | str | bool | float | int) -> View:
+        """コンポーネントを追加"""
         for c in args:
             self._components.append(c)
         return self
